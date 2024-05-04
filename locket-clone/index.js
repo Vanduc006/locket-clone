aa9f57bab45d6da59a97e30cd433e15904fe9a4d
Hi guys, my name is NGUYEN VAN DUC <98450746+Vanduc006@users.noreply.github.com>
2024-05-04 10:22:50 +0000

    fowts

---
+++ locket-clone/index.js
@@ -0,0 +1,7 @@
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+import App from './App.js';
+import Camera from './Camera.js'
+import Test from './Test.js'
+
+registerRootComponent(Test);
